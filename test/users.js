761be15ff6f6fd270536f78308c372cc6d97bbd0
Alberto García <albertoinformatica97@gmail.com>
2020-11-26 00:26:33 +0100

    test post user

--- test/users.js
+++ test/users.js
@@ -17,3 +17,53 @@ describe('/get', () => {
 		expect(Array.isArray(response.body.users)).to.equal(true)
 	})
 })
+
+describe('/post', () => {
+	it('should create and return user', async () => {
+		let email = getRandomEmail()
+		let userName = getRandomUserName()
+
+		const user = {
+			email: email,
+			password: 'qwerty',
+			name: userName,
+			userName: userName,
+		}
+
+		const response = await chai.request(app).post('/users').send(user)
+
+		expect(response.statusCode).to.equal(201)
+		response.body.should.have.property('user')
+		response.body.user.should.be.a('object')
+	})
+})
+
+function getRandomEmail() {
+	const letters = 'qwertyuiopasdfghjklzxcvbnm'
+	const emailLength = 10
+	let email = ''
+
+	for (let i = 0; i <= emailLength; i++) {
+		if (i === emailLength / 2) {
+			email += '@'
+		} else if (i === emailLength) {
+			email += '.es'
+		} else {
+			email += letters[Math.floor(Math.random() * letters.length - 1) + 1]
+		}
+	}
+
+	return email
+}
+
+function getRandomUserName() {
+	const letters = 'qwertyuiopasdfghjklzxcvbnm'
+	const userNameLength = 2
+	let userName = ''
+
+	for (let i = 0; i <= userNameLength; i++) {
+		userName += letters[Math.floor(Math.random() * letters.length - 1) + 1]
+	}
+
+	return userName
+}
