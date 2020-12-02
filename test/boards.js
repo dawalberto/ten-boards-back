bf7dc66090fcaae566d12a18a03ae96d0c5d2c24
Alberto García <albertoinformatica97@gmail.com>
2020-12-02 22:03:31 +0100

    post test for boards

--- test/boards.js
+++ test/boards.js
@@ -31,4 +31,37 @@ describe('☕️ boards', () => {
 				})
 		})
 	})
+
+	describe('/post okey', () => {
+		it('should create board and return http code 201', () => {
+			chai.request(app)
+				.post('/users/login')
+				.send({ email: 'alberto@alberto.es', password: 'qwerty' })
+				.end((err, res) => {
+					const token = res.body.token
+					const board = {
+						title: 'fake board',
+						description: 'fake board for test purpose',
+						totalTime: 0,
+						public: false,
+						finished: false,
+						user: '5fc7f2378882b116e9e37236',
+						dateAdded: new Date(),
+						dateUpdated: new Date(),
+						members: ['5fc7f2378882b116e9e37236'],
+						background: '#00a8ff',
+					}
+
+					chai.request(app)
+						.post('/boards')
+						.set('token', token)
+						.send(board)
+						.end((err, res) => {
+							expect(res.statusCode).to.equal(201)
+							done()
+						})
+					done()
+				})
+		})
+	})
 })
