3bd8c97a41ebe0a4d2ca025e68463d3ca0999167
Alberto García <albertoinformatica97@gmail.com>
2020-12-02 20:44:33 +0100

    initial get test to boards

---
+++ test/boards.js
@@ -0,0 +1,31 @@
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const app = require('../server/server')
+const expect = chai.expect
+
+chai.should()
+chai.use(chaiHttp)
+
+describe('/get okey', () => {
+	it('It should get the total number of boards and an array of boards the logged user belongs to.', () => {
+		chai.request(app)
+			.post('/users/login')
+			.send({ email: 'alberto@alberto.es', password: 'qwerty' })
+			.end((err, res) => {
+				const token = res.body.token
+
+				chai.request(app)
+					.get('/boards')
+					.set('token', token)
+					.end((err, res) => {
+						expect(res.statusCode).to.equal(200)
+						res.body.should.have.property('total')
+						res.body.should.have.property('boards')
+						expect(isNaN(res.body.total)).to.equal(false)
+						expect(Array.isArray(res.body.boards)).to.equal(true)
+
+						done()
+					})
+			})
+	})
+})
